a6d2e75e60a9c7fbb956918fb0f5cf70dbb11e48
creativeband <jaeil.han@creative-band.com>
2022-06-01 08:04:06 +0900

    Small Change

--- header.php
+++ header.php
@@ -50,14 +50,14 @@
 		<nav id="site-navigation" class="main-navigation">
 			<div class="inner">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rubberband' ); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
 			</div><!-- .inner -->
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
